9fb6d36fad747e5697a2546f82f6160751129411
Ben Daniels <benjamindaniels21@gmail.com>
2023-07-24 13:12:54 -0400

    logging form values to console

--- src/Components/AddUser/AddUser.jsx
+++ src/Components/AddUser/AddUser.jsx
@@ -6,19 +6,28 @@ import Button from '../Button/Button';
 const AddUser = (props) => {
 
   const [enteredUserName, setEnteredUserName] =useState('');
+  const [enteredUserAge, setEnteredUserAge] =useState('');
+
+  const userNameChangeHandler = (event) => {
+    setEnteredUserName(event.target.value);
+  }
+
+  const userAgeChangeHandler = (event) => {
+    setEnteredUserAge(event.target.value);
+  }
   
   const addUserHandler = (event) => {
     event.preventDefault();
-
+    console.log(enteredUserName, enteredUserAge);
   }
   
   return (
     <Card className={styles.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="">Username</label>
-        <input type="text" id='username'/>
+        <input type="text" id='username' onChange={userNameChangeHandler}/>
         <label htmlFor="age">Age (Years)</label>
-        <input type="number" id='age'/>
+        <input type="number" id='age'onChange={userAgeChangeHandler}/>
         <Button type='submit'>Add User</Button>
       </form>
     </Card>
